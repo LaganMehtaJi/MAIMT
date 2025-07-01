47694fa98c812dc9cb5b4fb14c36a36b1e76a5ca
Lagan Mehta <laganmehta.in@gmail.com>
2025-07-01 10:29:54 +0530

    Inatall Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import Hero from "../src/Components/Hero/index.jsx";
 import './App.css'
 
 function App() {
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-  
+   <Hero/>
     </>
   )
 }
